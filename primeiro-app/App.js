528399e021e2670cd1c71b4d2a60181511936bc8
João <joaovitorsenac7817@gmail.com>
2024-05-13 22:24:19 -0300

    feat: Teste com componentes

--- primeiro-app/App.js
+++ primeiro-app/App.js
@@ -1,22 +1,9 @@
-import { Platform, StyleSheet} from 'react-native';
-import { PageContainer, PageSubTitle, PageTitle, PageImage } from './src/class/styled-externo/styled-externo';
+import { CreativeComponent } from './src/class-component/native-components';
 
 // Outra forma de importar é: 
 // import * as S from './src/class/styled-externo/styled-externo';
 
 export default function App() {
-  return (
-    <PageContainer style={styles.container}>
-      <PageImage source={require('./src/img/palmeiras-sem-fundo.png')}></PageImage>
-      <PageTitle>Palmeiras</PageTitle>
-      <PageSubTitle>Melhor time do mundo!</PageSubTitle>
-    </PageContainer>
-  );
+  return <CreativeComponent />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Platform.OS === 'android' ?  '#CFE1FA' : '#fff'
-  },
-});
