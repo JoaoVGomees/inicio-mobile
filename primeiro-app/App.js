fd9b80cb5e8bfd283398425e2ac71470c839c01b
João <joaovitorsenac7817@gmail.com>
2024-05-25 12:19:22 -0300

    feat: create app-clima

--- primeiro-app/App.js
+++ primeiro-app/App.js
@@ -1,9 +1,9 @@
-import { CreativeComponent } from './src/class-component/native-components';
+import Home from "./src/api-clima/pages/Home/home";
 
 // Outra forma de importar é: 
 // import * as S from './src/class/styled-externo/styled-externo';
 
 export default function App() {
-  return <CreativeComponent />;
+  return <Home />;
 }
 
